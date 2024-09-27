7a829c1cb1233b2958c3a30cdc439948485096de
DESKTOP-176SM67\Kacper Żakieta 4Ti <k4sp3r.main@gmail.com>
2024-09-27 10:42:37 +0200

    Dodalem filmy strone rodzaje i w ogole mega cool uuh

--- filmy/src/App.js
+++ filmy/src/App.js
@@ -5,23 +5,22 @@ function App() {
   return (
     <div className="App">
       <form>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Tytuł filmu</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1"/>
-  </div>
-  <div class="mb-3 form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
-    </div>
+        <div>
+          <div class="mb-3">
+            <label for="film" class="form-label">Tytuł filmu:</label>
+            <input type="text" class="form-control" id="film"/>
+          </div>
+           <div id="emailHelp" class="form-text">Wybierz rodzaj filmu</div>
+        </div>
+        <div class="mb-3"><input type="email" class="form-control"/>
+        </div>
+        <div class="mb-3 form check">
+          <label class="form-check-label" for="exampleCheck1"></label>
+          </div>
+          <button type="sumbit" class="btn btn-primary">Sprawdź</button>
+          </form>
+          </div>
   );
 }
 
-export default App;
+  export default App;
