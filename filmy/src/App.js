445131ae2f67368bb7931f2458be4de3be79614b
DESKTOP-176SM67\Kacper Żakieta 4Ti <k4sp3r.main@gmail.com>
2024-09-27 11:05:11 +0200

    Cleanup

--- filmy/src/App.js
+++ filmy/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -8,25 +7,22 @@ function App() {
         <div>
           <div class="mb-3">
             <label for="film" class="form-label">Tytuł filmu:</label>
-            <input type="text" class="form-control" id="film"/>
+            <input type="text" class="form-control" id="film" />
           </div>
-          
-           <div class="mb-3">
-      <label for="disabledSelect" class="form-label">Rodzaj filmu</label>
-      <select id="disabledSelect" class="form-select">
-        <option>Pusta</option>
-        <option>Komedia</option>
-        <option>Obyczajowy</option>
-        <option>Sensacyjny</option>
-        <option>Horror</option>
-    
-      </select>
+          <div class="mb-3">
+            <label for="disabledSelect" class="form-label">Rodzaj filmu</label>
+            <select id="disabledSelect" class="form-select">
+              <option>Pusta</option>
+              <option>Komedia</option>
+              <option>Obyczajowy</option>
+              <option>Sensacyjny</option>
+              <option>Horror</option>
+            </select>
+          </div>
+        </div>
+        <button type="sumbit" class="btn btn-primary">Submit</button>
+      </form>
     </div>
-          </div>
-          <button type="sumbit" class="btn btn-primary">Submit</button>
-          </form>
-          </div>
   );
 }
-
-  export default App;
+export default App;
